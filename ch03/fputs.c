583204af3a67b8d8ffb5018bafce8d9ae1ffc8e2
Peter Olsen <pto@me.com>
2014-01-18 14:49:56 -0500

    fputs

---
+++ ch03/fputs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("usage: fputs <filename>\n");
+		exit(1);
+	}
+
+	FILE *stream = fopen(argv[1], "a");
+	if (!stream) {
+		perror("fopen");
+		exit(1);
+	}
+
+	if (fputs("The ship is made of wood.\n", stream) == EOF) {
+		perror("fputs");
+		exit(1);
+	}
+
+	if (fclose(stream) == EOF) {
+		perror("fclose");
+		exit(1);
+	}
+}
