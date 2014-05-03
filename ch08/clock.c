6b931bf67efc07f95dd2edbac37b607ee3bb2459
Peter Olsen <pto@me.com>
2014-05-03 19:57:37 -0400

    clock resolution

---
+++ ch08/clock.c
@@ -0,0 +1,27 @@
+#include <time.h>
+#include <stdio.h>
+
+clockid_t clocks[] = {
+	CLOCK_REALTIME,
+	CLOCK_MONOTONIC,
+	CLOCK_PROCESS_CPUTIME_ID,
+	CLOCK_THREAD_CPUTIME_ID,
+	CLOCK_MONOTONIC_RAW,
+	(clockid_t)-1
+};
+
+int main() {
+	int i;
+
+	for (i = 0; clocks[i] != (clockid_t)-1; i++) { 
+		struct timespec res;
+		int ret;
+
+		ret = clock_getres(clocks[i], &res);
+	   	if (ret)
+			perror ("clock_getres"); 
+		else
+			printf ("clock=%d sec=%ld nsec=%ld\n",
+				   	clocks[i], res.tv_sec, res.tv_nsec);
+	}
+}
